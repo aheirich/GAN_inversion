617a7e18ab3e2675f3349a98f6b4f05068157140
Alan Heirich <aheirich@slac.stanford.edu>
2018-07-24 11:36:21 -0700

    convert Z3 A3 Z4

---
+++ python/convert_preactivation_4.py
@@ -0,0 +1,33 @@
+
+#!/usr/bin/python3
+
+import sys
+
+data = []
+size = 1
+
+assert len(sys.argv) == 2
+file = open(sys.argv[1])
+
+for line in file:
+  words = line.strip().split(' ')
+  for word in words:
+    while word.startswith('['): word = word[1:]
+    while word.endswith(']'): word = word[:-1]
+    try:
+      value = float(word)
+      data.append(word)
+    except ValueError:
+      pass
+
+string = 'param Z4 :'
+for i in range(size): string = string + ' ' + str(i + 1)
+string = string + ' :='
+print(string)
+
+line = 1
+for datum in data:
+  print(line, datum)
+  line = line + 1
+
+print(';')
