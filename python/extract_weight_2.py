1e0cdb4805210366872f052190e4c358ae114099
Alan Heirich <heirich@alumni.caltech.edu>
2018-09-27 14:24:03 -0700

    extract weight 2 to reverse engineer the tensor flow behavior

---
+++ python/extract_weight_2.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+import sys
+
+numFilters = 32
+filterWidth = 3
+filterHeight = 8
+filterDepth = 16
+
+filterCount = 1
+heightCount = 1
+depthCount = 1
+
+print("param weight_2 :=")
+
+for line in sys.stdin:
+    allWords = line.strip().split(' ')
+    words = []
+    for word in allWords:
+        if word != '':
+            words.append(word)
+    if len(words) < 3:
+        continue
+    if words[0] == '[' or words[0] == '[[':
+        words = words[1:]
+    while words[0].startswith('['):
+        words[0] = words[0][1:]
+    if words[-1] == ']' or words[-1] == ']]':
+        words = words[:-1]
+    while words[-1][-1] == ']':
+        words[-1] = words[-1][:-1]
+    if len(words) != 3:
+        print('line not 3', line)
+        print('words not 3', words)
+    assert len(words) == 3
+
+    print('[', str(words[0]) + ', ', ])
+
+    if heightCount == 1:
+        depthCount = depthCount + 1
+        if depthCount > filterDepth:
+            depthCount = 1
+            filterCount = filterCount + 1
+
+    heightCount = heightCount + 1
+    if heightCount > filterHeight: heightCount = 1
+
+print(";\n")
